2772e95f1a143fa7af471e454b5f2a26eab8a677
walnuts1018 <walnuts1018@hatena.com>
2025-06-27 03:48:57 +0900

    feat: Refactor OIDCConfig to include OIDC import and add OpenID scope
    Signed-off-by: walnuts1018 <walnuts1018@hatena.com>

--- config/oidc.go
+++ config/oidc.go
@@ -1,14 +1,18 @@
 package config
 
-import "golang.org/x/oauth2"
+import (
+	"github.com/coreos/go-oidc/v3/oidc"
+	"golang.org/x/oauth2"
+)
 
 type OIDCConfig struct {
-	IssuerURL    string `env:"OIDC_ISSUER_URL,required"`
-	ClientID     string `env:"OIDC_CLIENT_ID,required"`
-	ClientSecret string `env:"OIDC_CLIENT_SECRET,required"`
-	RedirectURL  string `env:"OIDC_REDIRECT_URL,required"`
-	AllowedGroup string `env:"OIDC_ALLOWED_GROUPS,required"`
-	GroupClaim   string `env:"OIDC_GROUP_CLAIM" envDefault:"groups"`
+	IssuerURL        string   `env:"OIDC_ISSUER_URL,required"`
+	ClientID         string   `env:"OIDC_CLIENT_ID,required"`
+	ClientSecret     string   `env:"OIDC_CLIENT_SECRET,required"`
+	RedirectURL      string   `env:"OIDC_REDIRECT_URL,required"`
+	AllowedGroup     string   `env:"OIDC_ALLOWED_GROUPS,required"`
+	GroupClaim       string   `env:"OIDC_GROUP_CLAIM" envDefault:"groups"`
+	AdditionalScopes []string `env:"OIDC_ADDITIONAL_SCOPES" envDefault:""`
 }
 
 func (c *OIDCConfig) ToOAuth2Config() oauth2.Config {
@@ -16,5 +20,6 @@ func (c *OIDCConfig) ToOAuth2Config() oauth2.Config {
 		ClientID:     c.ClientID,
 		ClientSecret: c.ClientSecret,
 		RedirectURL:  c.RedirectURL,
+		Scopes:       append([]string{oidc.ScopeOpenID}, c.AdditionalScopes...),
 	}
 }
